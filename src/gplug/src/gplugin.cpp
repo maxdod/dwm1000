948b20a7a50cd354e5a9630711936fb81614bd05
Massimo <39596051+maxdod@users.noreply.github.com>
2023-01-11 01:40:38 -0800

    Add files via upload

---
+++ src/gplug/src/gplugin.cpp
@@ -0,0 +1,287 @@
+/*
+ * Copyright (C) 2016 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <functional>
+
+#include "../include/ignition/math.hh"
+//#include "../include/physics/physics.hh"
+#include "../include/ActorPlugin.hh"
+#include "../include/askPosition.h"
+
+
+
+using namespace gazebo;
+GZ_REGISTER_MODEL_PLUGIN(ActorPlugin)
+
+#define WALKING_ANIMATION "walking"
+
+
+void ActorPlugin::PosCallback(const std_msgs::Float64MultiArray::ConstPtr& msg)
+{
+  int i = 0;
+  //std::cout <<  "Passo qui" << std::endl;
+  for (std::vector<double>::const_iterator it = msg->data.begin();it != msg->data.end(); ++it)
+  {
+    Coords[i] = *it;
+     //std::cout <<  Coords[i] << std::endl;
+    i++;
+  }
+
+}
+/////////////////////////////////////////////////
+ActorPlugin::ActorPlugin()
+{
+ 
+}
+
+/////////////////////////////////////////////////
+void ActorPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+{
+  this->sdf = _sdf;
+  this->actor = boost::dynamic_pointer_cast<physics::Actor>(_model);
+  this->world = this->actor->GetWorld();
+
+  this->connections.push_back(event::Events::ConnectWorldUpdateBegin(
+          std::bind(&ActorPlugin::OnUpdate, this, std::placeholders::_1)));
+
+  this->Reset();
+
+  // Read in the target weight
+  if (_sdf->HasElement("target_weight"))
+    this->targetWeight = _sdf->Get<double>("target_weight");
+  else
+    this->targetWeight = 1.15;
+
+  // Read in the obstacle weight
+  if (_sdf->HasElement("obstacle_weight"))
+    this->obstacleWeight = _sdf->Get<double>("obstacle_weight");
+  else
+    this->obstacleWeight = 1.5;
+
+  // Read in the animation factor (applied in the OnUpdate function).
+  if (_sdf->HasElement("animation_factor"))
+    this->animationFactor = _sdf->Get<double>("animation_factor");
+  else
+    this->animationFactor = 4.5;
+
+  // Add our own name to models we should ignore when avoiding obstacles.
+  this->ignoreModels.push_back(this->actor->GetName());
+
+  // Read in the other obstacles to ignore
+  if (_sdf->HasElement("ignore_obstacles"))
+  {
+    sdf::ElementPtr modelElem =
+      _sdf->GetElement("ignore_obstacles")->GetElement("model");
+    while (modelElem)
+    {
+      this->ignoreModels.push_back(modelElem->Get<std::string>());
+      modelElem = modelElem->GetNextElement("model");
+    }
+  }
+  /***********************ROS *************/
+   if (!ros::isInitialized()) {
+  int argc = 0;
+  char ** argv = NULL;
+  ros::init(argc, argv, "gazebo_client", ros::init_options::NoSigintHandler);
+  }
+    this->nh.reset(new ros::NodeHandle("gazebo_client"));
+    this->client = this->nh->serviceClient<localizer_dwm1001::askPosition>("/getPosition");
+    //std::cout << "ROS inizializzato" << std::endl;
+
+    //ROS Subscriber to Radio Command. Define the subscriber parameters and then sub.
+    ros::SubscribeOptions so =
+      ros::SubscribeOptions::create<std_msgs::Float64MultiArray>(
+        "/dwm1000/pos" ,
+        1,
+        boost::bind(&ActorPlugin::PosCallback, this, _1),
+        ros::VoidPtr(), &this->rosQueue);
+        this->rosQueueThread = std::thread(std::bind(&ActorPlugin::QueueThread, this));
+    this->sub_pos_cmd = this->nh->subscribe(so);
+    
+  
+    //this->sub_pos_cmd = this->nh->subscribe("/dwm1000/pos",100,PosCallback);
+
+}
+void ActorPlugin::QueueThread() {
+    static const double timeout = 0.01;
+    while(this->nh->ok())
+    {
+      this->rosQueue.callAvailable(ros::WallDuration(timeout));
+    }
+  }
+/////////////////////////////////////////////////
+void ActorPlugin::Reset()
+{
+  this->velocity = 0.8;
+  this->lastUpdate = 0;
+
+  if (this->sdf && this->sdf->HasElement("target"))
+    this->target = this->sdf->Get<ignition::math::Vector3d>("target");
+  else
+    this->target = ignition::math::Vector3d(0, -5, 1.2138);
+
+  auto skelAnims = this->actor->SkeletonAnimations();
+  if (skelAnims.find(WALKING_ANIMATION) == skelAnims.end())
+  {
+    gzerr << "Skeleton animation " << WALKING_ANIMATION << " not found.\n";
+  }
+  else
+  {
+    // Create custom trajectory
+    this->trajectoryInfo.reset(new physics::TrajectoryInfo());
+    this->trajectoryInfo->type = WALKING_ANIMATION;
+    this->trajectoryInfo->duration = 1.0;
+
+    this->actor->SetCustomTrajectory(this->trajectoryInfo);
+  }
+  
+}
+
+/////////////////////////////////////////////////
+void ActorPlugin::ChooseNewTarget()
+{
+  ignition::math::Vector3d newTarget(this->target);
+ //std::cout << "chiedi nuovo target" << std::endl;
+  
+  
+  if ((newTarget - this->target).Length() < 0.3)
+  {
+    //newTarget.X(ignition::math::Rand::DblUniform(-3, 3.5));
+    //newTarget.Y(ignition::math::Rand::DblUniform(-10, 2));
+    //newTarget.X(Coords[0]);
+    //newTarget.Y(Coords[1]);
+    //std::cout << Coords[0] << std::endl;
+    
+    localizer_dwm1001::askPosition srv;
+    if (this->client.call(srv))
+  {
+    //std::cout << srv.response.posX << " "  << srv.response.posY << std::endl;
+    if (this->xPos != srv.response.posX && this->yPos != srv.response.posY)
+        {
+          this->xPos = srv.response.posX;
+          this->yPos = srv.response.posY;
+          newTarget.X(this->xPos);
+          newTarget.Y(this->yPos); 
+        } else return;
+
+    
+  }
+  else
+  {
+    //ROS_ERROR("Failed to call service add_two_ints");
+    return ;
+  }
+
+    for (unsigned int i = 0; i < this->world->ModelCount(); ++i)
+    {
+      double dist = (this->world->ModelByIndex(i)->WorldPose().Pos()
+          - newTarget).Length();
+      if (dist < 0.3)
+      {
+        newTarget = this->target;
+        break;
+      }
+    }
+  }
+  this->target = newTarget;
+}
+
+/////////////////////////////////////////////////
+void ActorPlugin::HandleObstacles(ignition::math::Vector3d &_pos)
+{
+  ros::spinOnce();
+  for (unsigned int i = 0; i < this->world->ModelCount(); ++i)
+  {
+    physics::ModelPtr model = this->world->ModelByIndex(i);
+    if (std::find(this->ignoreModels.begin(), this->ignoreModels.end(),
+          model->GetName()) == this->ignoreModels.end())
+    {
+      ignition::math::Vector3d offset = model->WorldPose().Pos() -
+        this->actor->WorldPose().Pos();
+      double modelDist = offset.Length();
+      if (modelDist < 4.0)
+      {
+        double invModelDist = this->obstacleWeight / modelDist;
+        offset.Normalize();
+        offset *= invModelDist;
+        _pos -= offset;
+      }
+    }
+  }
+}
+
+/////////////////////////////////////////////////
+void ActorPlugin::OnUpdate(const common::UpdateInfo &_info)
+{
+  // Time delta
+  double dt = (_info.simTime - this->lastUpdate).Double();
+
+  ignition::math::Pose3d pose = this->actor->WorldPose();
+  ignition::math::Vector3d pos = this->target - pose.Pos();
+  ignition::math::Vector3d rpy = pose.Rot().Euler();
+
+  double distance = pos.Length();
+
+  // Choose a new target position if the actor has reached its current
+  // target.
+  //std::cout << "target = " << this->target << std::endl;
+  //std::cout << "target = " << pose.Pos() << std::endl;
+  if (distance < 0.3)
+  {
+    //std::cout << "distanza = " << distance << std::endl;
+    this->ChooseNewTarget();
+    pos = this->target - pose.Pos();
+  }
+
+  // Normalize the direction vector, and apply the target weight
+  pos = pos.Normalize() * this->targetWeight;
+
+  // Adjust the direction vector by avoiding obstacles
+  this->HandleObstacles(pos);
+
+  // Compute the yaw orientation
+  ignition::math::Angle yaw = atan2(pos.Y(), pos.X()) + 1.5707 - rpy.Z();
+  yaw.Normalize();
+
+  // Rotate in place, instead of jumping.
+  if (std::abs(yaw.Radian()) > IGN_DTOR(10))
+  {
+    pose.Rot() = ignition::math::Quaterniond(1.5707, 0, rpy.Z()+
+        yaw.Radian()*0.001);
+  }
+  else
+  {
+    pose.Pos() += pos * this->velocity * dt;
+    pose.Rot() = ignition::math::Quaterniond(1.5707, 0, rpy.Z()+yaw.Radian());
+  }
+
+  // Make sure the actor stays within bounds
+  pose.Pos().X(std::max(-40.0, std::min(10.5, pose.Pos().X())));
+  pose.Pos().Y(std::max(-40.0, std::min(10.5, pose.Pos().Y())));
+  pose.Pos().Z(1.2138);
+
+  // Distance traveled is used to coordinate motion with the walking
+  // animation
+  double distanceTraveled = (pose.Pos() -
+      this->actor->WorldPose().Pos()).Length();
+
+  this->actor->SetWorldPose(pose, false, false);
+  this->actor->SetScriptTime(this->actor->ScriptTime() +
+    (distanceTraveled * this->animationFactor));
+  this->lastUpdate = _info.simTime;
+  ros::spinOnce();
+}
